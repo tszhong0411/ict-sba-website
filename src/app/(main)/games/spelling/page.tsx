ffa7497c7b314e3e64c015f8d26c9762869ff600
tszhong0411 <me@honghong.me>
2023-11-15 09:05:13 +0800

    feat: update

--- src/app/(main)/games/spelling/page.tsx
+++ src/app/(main)/games/spelling/page.tsx
@@ -1,8 +1,5 @@
-'use client'
-
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
 import PageTitle from '@/components/page-title'
 import { buttonVariants } from '@/components/ui/button'
