cadbbba00d062a327e90804b7d52158c84e84f67
tszhong0411 <me@honghong.me>
2023-12-10 23:19:32 +0800

    feat: update feedback form

--- src/app/(main)/feedback/page.tsx
+++ src/app/(main)/feedback/page.tsx
@@ -14,17 +14,21 @@ const FeedbackPage = () => {
       <PageTitle title='反饋' />
       <form
         action='mailto:me@honghong.me'
-        method='POST'
+        method='GET'
         encType='text/plain'
         className='mx-auto max-w-2xl space-y-4'
       >
         <div className='grid items-center gap-1.5'>
-          <Label htmlFor='name'>名字</Label>
-          <Input type='text' id='name' name='name' />
+          <Label htmlFor='email'>電子郵件</Label>
+          <Input type='text' id='email' name='email' />
         </div>
         <div className='grid items-center gap-1.5'>
-          <Label htmlFor='feedback'>留言</Label>
-          <Textarea id='feedback' name='feedback' rows={20} />
+          <Label htmlFor='subject'>標題</Label>
+          <Input type='text' id='subject' name='subject' />
+        </div>
+        <div className='grid items-center gap-1.5'>
+          <Label htmlFor='body'>留言</Label>
+          <Textarea id='body' name='body' rows={20} />
         </div>
         <Button type='submit'>提交</Button>
       </form>
